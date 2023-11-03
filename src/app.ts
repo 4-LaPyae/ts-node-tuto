73758173d990ac0c40f5d664fa34aada2225eaae
lapyae <lapyae2022.gm@gmail.com>
2023-11-03 13:08:03 +0630

    if database is connected,server is running

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import { errorHandler } from "./middleware/errorhandler";
 import { connectToDatabase } from "./config/connect";
 import cors from "cors";
 const app = express();
-connectToDatabase();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", todoRouter);
 app.use(errorHandler);
-app.listen(4000, () => console.log("Server running on port 4000"));
+connectToDatabase()
+  .then((res) => {
+    app.listen(4000, () => console.log("Server running on port 4000"));
+  })
+  .catch((err) => console.log(err));
